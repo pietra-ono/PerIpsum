cff6a8805711fc8cb59d10c78f6ee412af9c1a11
Bruno <brunozanferrari12@gmail.com>
2024-11-23 15:36:03 -0300

    LICENSE e READ.ME

--- PerIpsumOriginal/PerIpsumOriginal/obj/Debug/net8.0/PerIpsumOriginal.AssemblyInfo.cs
+++ PerIpsumOriginal/PerIpsumOriginal/obj/Debug/net8.0/PerIpsumOriginal.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerIpsumOriginal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804c914f403fd7564af1c4db3db564d61baee4d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerIpsumOriginal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerIpsumOriginal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
